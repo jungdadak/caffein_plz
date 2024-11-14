efb0fd7ce4cdf9c614644d6aab02828f7676e4ff
Andy_Lee <120154249+jungdadak@users.noreply.github.com>
2024-11-14 09:02:03 +0900

    sadf

--- app/page.js
+++ app/page.js
@@ -1,21 +1,22 @@
+// app/Anal/page.js
 'use client';
 import DragAndDropUpload from '@/components/Dropzone';
 import MainDashboard from '@/components/MainDash';
 import Image from 'next/image';
 
-export default function Home() {
+export default function AnalPage() {
   return (
-    <div className="p-3 w-full rounded-sm">
+    <div className="min-h-screen bg-gradient-to-r from-gray-800 to-gray-900 p-6 flex flex-col">
       <Image
         src="/Typo.png"
         alt="logo"
         width={400}
         height={300}
-        className="rounded-md mx-auto mt-8"
+        className="rounded-md mx-auto mt-8 shadow-lg"
       />
-      <div className="mt-12">
-        <div className="flex w-full max-w-screen-lg justify-between m-auto mt-2 mb-6 shadow-xl p-4 rounded-md bg-opacity-60 backdrop-blur-sm bg-gray-900">
-          <div className="shadow-xl p-2 rounded-md bg-opacity-60 backdrop-blur-sm bg-gray-900">
+      <div className="mt-12 flex-1">
+        <div className="flex w-full max-w-screen-lg justify-center m-auto mt-2 mb-6 shadow-2xl p-6 rounded-lg bg-gray-800 bg-opacity-75 backdrop-blur-sm h-full">
+          <div className="w-full h-full">
             <DragAndDropUpload />
           </div>
         </div>
