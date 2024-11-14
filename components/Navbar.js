4089e566cd4e89c3a8f06a1355d15cc2444ee93e
Andy_Lee <120154249+jungdadak@users.noreply.github.com>
2024-11-14 10:20:37 +0900

    sdfdasf

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,6 +4,7 @@
 // 이미지 파일 임포트
 import Image from 'next/image';
 import { useState, useEffect } from 'react';
+import { FaSpinner } from 'react-icons/fa'; // 스피너 아이콘 임포트
 import logo from '/public/logo.ico';
 import nextjs from '/public/nextjslogo.jpeg';
 import cuda from '/public/cudalogo.png';
@@ -20,6 +21,8 @@ export default function Navbar({
 }) {
   const [id, setId] = useState('');
   const [password, setPassword] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   // 다크 모드에 따라 전체 페이지 배경색 변경
   useEffect(() => {
@@ -33,6 +36,28 @@ export default function Navbar({
       }
     }
   }, [isDarkMode]);
+
+  // 로그인 처리 함수
+  const handleLogin = (e) => {
+    e.preventDefault(); // 폼 제출 방지
+    setIsLoading(true);
+    // 로딩 상태를 2초 동안 유지한 후 로그인 처리
+    setTimeout(() => {
+      if (id === 'admin' && password === 'admin') {
+        setIsLoggedIn(true);
+      } else {
+        alert('Invalid credentials');
+      }
+      setIsLoading(false);
+    }, 2000); // 2초 딜레이
+  };
+
+  // 로그아웃 처리 함수
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    setId('');
+    setPassword('');
+  };
 
   return (
     <div className="overflow-y-auto z-50">
@@ -74,58 +99,93 @@ export default function Navbar({
                   isDarkMode ? 'text-white' : 'text-black'
                 }`}
               >
-                {/* 로그인 섹션 */}
-                <h1 className="m-3 text-left shadow-lg block">Please Log In</h1>
-                <form
-                  id="login-nav"
-                  className={`flex flex-col p-2 border-b-2 border-t-2 rounded-md m-1 ${
-                    isDarkMode
-                      ? 'bg-gray-800 border-gray-600 text-white'
-                      : 'bg-gray-300 border-gray-400 text-black'
-                  }`}
-                  action={'/api/login'}
-                >
-                  <input
-                    type="text"
-                    id="id"
-                    placeholder="Enter ID"
-                    className={`p-2 m-1 rounded-md text-center ${
-                      isDarkMode
-                        ? 'bg-gray-800 text-white'
-                        : 'bg-gray-200 text-black'
-                    }`}
-                    onChange={(e) => setId(e.target.value)}
-                  />
-                  <input
-                    type="password"
-                    id="password"
-                    placeholder="Enter Password"
-                    className={`p-2 m-1 rounded-md text-center ${
-                      isDarkMode
-                        ? 'bg-gray-800 text-white'
-                        : 'bg-gray-200 text-black'
-                    }`}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                  <button
-                    className={`p-2 m-1 rounded-md text-xs text-center border-2 ${
-                      isDarkMode
-                        ? 'bg-black text-red-500 border-red-800'
-                        : 'bg-gray-400 text-red-600 border-red-400'
-                    }`}
-                    type="submit"
-                  >
-                    Log In
-                  </button>
-                  <a
-                    className={`text-left ${
-                      isDarkMode ? 'text-blue-300' : 'text-blue-500'
-                    }`}
-                    href="#"
-                  >
-                    Sign Up
-                  </a>
-                </form>
+                {!isLoggedIn ? (
+                  <>
+                    {/* 로그인 섹션 */}
+                    <h1 className="m-3 text-left shadow-lg block">
+                      Please Log In
+                    </h1>
+                    <form
+                      id="login-nav"
+                      className={`flex flex-col p-2 border-b-2 border-t-2 rounded-md m-1 ${
+                        isDarkMode
+                          ? 'bg-gray-800 border-gray-600 text-white'
+                          : 'bg-gray-300 border-gray-400 text-black'
+                      }`}
+                      onSubmit={handleLogin}
+                    >
+                      <input
+                        type="text"
+                        id="id"
+                        placeholder="Enter ID"
+                        className={`p-2 m-1 rounded-md text-center ${
+                          isDarkMode
+                            ? 'bg-gray-800 text-white'
+                            : 'bg-gray-200 text-black'
+                        }`}
+                        onChange={(e) => setId(e.target.value)}
+                        value={id}
+                        disabled={isLoading}
+                      />
+                      <input
+                        type="password"
+                        id="password"
+                        placeholder="Enter Password"
+                        className={`p-2 m-1 rounded-md text-center ${
+                          isDarkMode
+                            ? 'bg-gray-800 text-white'
+                            : 'bg-gray-200 text-black'
+                        }`}
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password}
+                        disabled={isLoading}
+                      />
+                      <button
+                        className={`p-2 m-1 rounded-md text-xs text-center border-2 flex items-center justify-center ${
+                          isDarkMode
+                            ? 'bg-black text-red-500 border-red-800'
+                            : 'bg-gray-400 text-red-600 border-red-400'
+                        }`}
+                        type="submit"
+                        disabled={isLoading}
+                      >
+                        {isLoading ? (
+                          <>
+                            <FaSpinner className="animate-spin mr-2" />
+                            Loading...
+                          </>
+                        ) : (
+                          'Log In'
+                        )}
+                      </button>
+                      <a
+                        className={`text-left ${
+                          isDarkMode ? 'text-blue-300' : 'text-blue-500'
+                        }`}
+                        href="#"
+                      >
+                        Sign Up
+                      </a>
+                    </form>
+                  </>
+                ) : (
+                  <>
+                    {/* 로그인된 상태의 UI */}
+                    <h1 className="m-3 text-left shadow-lg block">
+                      Welcome, {id}!
+                    </h1>
+                    <button
+                      className={`p-2 m-1 rounded-md text-xs text-center border-2 ${
+                        isDarkMode
+                          ? 'bg-black text-red-500 border-red-800'
+                          : 'bg-gray-400 text-red-600 border-red-400'
+                      }`}
+                      onClick={handleLogout}
+                    >
+                      Log Out
+                    </button>
+                  </>
+                )}
 
                 {/* Start Guide 섹션 */}
                 <h1 className="mx-3 text-left shadow-lg block">Start Guide</h1>
@@ -179,7 +239,7 @@ export default function Navbar({
                       href="https://your-my-page-link.com"
                       className="text-blue-400 underline"
                     >
-                      my
+                      my page
                     </a>
                     .
                   </p>
